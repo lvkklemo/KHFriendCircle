ad8ed7de69ef20b4e3c90950d09bbcaa4f578591
lkk2016 <631813042@qq.com>
2017-02-22 11:23:36 +0800

    1,初始化项目

---
+++ KHFriendCircle/KHFriendCircle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KHFriendCircle
+//
+//  Created by 宇航 on 17/2/22.
+//  Copyright © 2017年 KH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
